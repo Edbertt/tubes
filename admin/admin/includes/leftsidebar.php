671d7755475c50d4cf39cd754e34ab831b1be256
Edbertt <edbert1xie@gmail.com>
2021-12-07 21:53:15 +0700

    File css , js , footer , leftsidebar, topbar, dashboard admin, manage akun , dan update delete

---
+++ admin/admin/includes/leftsidebar.php
@@ -0,0 +1,55 @@
+<div class="left side-menu">
+                <div class="sidebar-inner slimscrollleft">
+
+                    <!--- Sidemenu -->
+                    <div id="sidebar-menu">
+                        <ul>
+                        	<li class="menu-title">Navigation</li>
+
+                            <li class="has_sub">
+                                <a href="dashboard.php" class="waves-effect"><i class="mdi mdi-view-dashboard"></i> <span> Dashboard </span> </a>
+                         
+                            </li>
+  <li class="has_sub">
+                                <a href="manage-subadmins.php" class="waves-effect"><i class="fas fa-users"></i> <span> Manage Akun </span></a>
+                                
+                            </li>               
+
+
+                            <li class="has_sub">
+                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span> Kategori </span> <span class="menu-arrow"></span></a>
+                                <ul class="list-unstyled">
+                                	<li><a href="add-category.php">Tambah Kategori</a></li>
+                                    <li><a href="manage-categories.php">Manage Kategori</a></li>
+                                </ul>
+                            </li>
+         
+  <li class="has_sub">
+                                <a href="javascript:void(0);" class="waves-effect"><i class="far fa-newspaper"></i> <span> Berita </span> <span class="menu-arrow"></span></a>
+                                <ul class="list-unstyled">
+                                    <li><a href="add-post.php">Tambah Berita</a></li>
+                                    <li><a href="manage-posts.php">Manage Berita</a></li>
+                                     <li><a href="trash-posts.php">Recycle Bin</a></li>
+                                </ul>
+                            </li>  
+                     
+
+                            
+   <li class="has_sub">
+                                <a href="javascript:void(0);" class="waves-effect"><i class="fas fa-comments"></i> <span> Komentar </span> <span class="menu-arrow"></span></a>
+                                <ul class="list-unstyled">
+                                  <li><a href="unapprove-comment.php">Waiting for Approval </a></li>
+                                    <li><a href="manage-comments.php">Approved</a></li>
+                                </ul>
+                            </li>   
+
+                        </ul>
+                    </div>
+                    <!-- Sidebar -->
+                    <div class="clearfix"></div>
+
+
+                </div>
+                <!-- Sidebar -left -->
+
+            </div>
