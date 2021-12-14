9f62927ca5504779380f8d066e9045bbbd2cc5c7
Marianatio <natiomaria@gmail.com>
2021-12-15 00:10:47 +0700

    halaman untuk login

--- login/login.php
+++ login/login.php
@@ -6,10 +6,12 @@
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<title>My Login Page</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="../css/my-login.css">
+	<link rel="stylesheet" type="text/css" href="css/my-login.css">
+	<link rel="shortcut icon" href="../assets/images/icon.jpg" type="image/x-icon">
+
 	<style>
 		body{
-			background-image : url("../css/image/bg.jpg");
+			background-image : url("css/image/bg.jpg");
 			background-repeat: no-repeat;
 			background-size: cover;
 			background-attachment: fixed;
@@ -34,16 +36,16 @@
 									$user_login = $_POST['username'];
 									$pass_login = md5($_POST['password']);
 
-									$sql = "SELECT * FROM akun WHERE AdminUserName = '{$user_login}' and AdminPassword = '{$pass_login}'";
+									$sql = "SELECT * FROM akun WHERE username = '{$user_login}' and password = '{$pass_login}'";
 									$query = mysqli_query($con, $sql);
 
 									while ($row = mysqli_fetch_array($query))
 									{
 										$namad = $row['nama_depan'];
 										$namab = $row['nama_belakang'];
-										$user = $row['AdminUserName'];
-										$pass = $row['AdminPassword'];
-										$mail = $row['AdminEmailId'];
+										$user = $row['username'];
+										$pass = $row['password'];
+										$mail = $row['email'];
         								$hp = $row['no_hp'];
         								$clearence = $row['userType'];
 										$status = $row['status'];
@@ -61,7 +63,7 @@
 											$_SESSION['userType'] = $clearence;
 										}
 										else{
-											header("Location:../test/user.php");
+											header("Location:../user.php");
 											$_SESSION['username'] = $user;
 											$_SESSION['nama_depan'] = $namad;
 											$_SESSION['nama_belakang'] = $namab;
@@ -80,8 +82,6 @@
 									}
 									else{
 										$errors['email'] = "It's look like you're not yet a member! Click on the bottom link to signup.";
-										// echo $pass;
-										// echo $pass_login;
 									}
 								}
 							?>
@@ -139,6 +139,6 @@
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-	<script src="../js/my-login.js"></script>
+	<script src="js/my-login.js"></script>
 </body>
 </html>
